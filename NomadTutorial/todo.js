63274a047c4e596fd229bacf63610a0ab83e36e9
kon6443@gmail.com <kon6443@gmail.com>
2020-12-21 22:36:55 -0600

    NomadTutorial_JS

---
+++ NomadTutorial/todo.js
@@ -0,0 +1,28 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function paintToDo(text) {
+  console.log(text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintGreeting(currentValue);
+}
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
