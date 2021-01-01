feab567b731fccaee5541239286b775dda0e9423
kon6443@gmail.com <kon6443@gmail.com>
2021-01-01 16:38:23 -0600

    Working on it

--- Snake/snake.js
+++ Snake/snake.js
@@ -11,14 +11,57 @@ let snakeColor = "#ED5B5B",
   tileColor = "#EEEEEE";
 (wallColor = "#2E2E2E"), (coinColor = "#4476C6");
 
+function move(direction) {
+  switch (direction) {
+    case 0:
+      y += 1;
+      break;
+    case 1:
+      y -= 1;
+      break;
+    case 2:
+      x -= 1;
+      break;
+    case 3:
+      x += 1;
+      break;
+    default:
+      return;
+  }
+  if(isInvalidMove(x, y)) gameOver();
+  setSnake(x, y);
+  meetCoin();
+  scoring();
+}
+
+/*
+function removeSnake() {
+    let ty = snakeQueue[0][];
+    //let tx = snakeQueue[][];
+}
+*/
+
+function setSnake(x, y) {
+  snakeQueue.push(new Array(x, y));
+  document.getElementById(
+    String(x) + " " + String(y)
+  ).style.background = snakeColor;
+}
+
 function drawWall() {
   let wallCell = new Array();
-  for(let i=0;i<MY;i++) wallCell.push(new Array(i, 0));
-  for(let i=0;i<MY;i++) wallCell.push(new Array(i, MX-1));
-  for(let i=0;i<MX;i++) wallCell.push(new Array(0, i));
-  for(let i=0;i<MX;i++) wallCell.push(new Array(MY-1, i));
-  for(let i=0;i<wallCell.length;i++) {
-    console.log(wallCell[i][1]);
+  for (let i = 0; i < MY; i++) wallCell.push(new Array(i, 0));
+  for (let i = 0; i < MY; i++) wallCell.push(new Array(i, MX - 1));
+  for (let i = 0; i < MX; i++) wallCell.push(new Array(0, i));
+  for (let i = 0; i < MX; i++) wallCell.push(new Array(MY - 1, i));
+  for (let i = 0; i < wallCell.length; i++) {
+    let wy = wallCell[i][0];
+    let wx = wallCell[i][1];
+    document.getElementById(
+      String(wy) + " " + String(wx)
+    ).style.background = wallColor;
+    document.getElementById(String(wy) + " " + String(wx)).style.borderRadius =
+      "1.5px";
   }
 }
 
@@ -36,25 +79,23 @@ function drawBoard() {
 
 document.onkeydown = keyDownEventHandler;
 function keyDownEventHandler(e) {
-  switch (e.keyCode) {
-    case 38:
-      moveDir(0);
-      break; //up
-    case 40:
-      moveDir(1);
-      break; //down
-    case 37:
-      moveDir(2);
-      break; //left
-    case 39:
-      moveDir(3);
-      break; //right
-  }
+    if((e.keyCode === 38) && (direction !== 1)) direction = 0;
+    else if((e.keyCode === 40) && (direction !== 0)) direction = 1;
+    else if((e.keyCode === 37) && (direction !== 3)) direction = 2;
+    else if((e.keyCode === 39) && (direction !== 2)) direction = 3;
 }
 
 function init() {
   drawBoard();
   drawWall();
+  y = parseInt(MY / 2);
+  x = parseInt(MX / 2);
+  setSnake(x, y);
+  setCoin();
+  score = 0;
+  direction = -1;
+  speed = 75;
+  keepMove = setInterval("move(direction)", speed);
 }
 
 init();
