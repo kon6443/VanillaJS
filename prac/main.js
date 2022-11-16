2bdc877f7abd23324829b6110b452b81489982c3
Onam Kwon <kon6443@gmail.com>
2022-11-16 13:36:09 +0900

    Practiced clean code

--- prac/main.js
+++ prac/main.js
@@ -1,12 +1,12 @@
 // 안내
 // 주석 아래에 요구사항에 맞는 코드를 작성해주세요.
-
-// 요구사항
-// 1부터 10까지 순회하면서 숫자를 출력하세요.
-// 3, 4, 7 에서는 숫자와 함께 "Hello, world!"를 출력하세요.
-// 7에서는 "Hello, world!" 대신 "Bye, world!"가 출력되도록 수정하세요.
-// 4에서는 숫자가 출력되지 않게 수정하세요
-
+/** 
+ * 요구사항
+ * 1. 1부터 10까지 순회하면서 숫자를 출력하세요.
+ * 2. 3, 4, 7 에서는 숫자와 함께 "Hello, world!"를 출력하세요.
+ * 3. 7에서는 "Hello, world!" 대신 "Bye, world!"가 출력되도록 수정하세요.
+ * 4. 4에서는 숫자가 출력되지 않게 수정하세요
+*/
 // 코드 작성
 
 const caseMap = {
@@ -15,16 +15,17 @@ const caseMap = {
     7: "Bye, World!",
 }
 
-function getGreetingMessage(caseNumber) {
-    return caseMap[caseNumber];
+function printGreetingMessage(caseNumber) {
+    if(caseMap[caseNumber]===undefined) {
+        console.log(caseNumber);
+    } else {
+        console.log(caseMap[caseNumber]);
+    }
+    
 }
 
 for(let i=1;i<11;i++) {
-    if(getGreetingMessage(i)===undefined) {
-        console.log(i);
-    } else {
-        console.log(getGreetingMessage(i));
-    }
+    printGreetingMessage(i);
 }
 
-console.log("The end..");
+console.log("The end.....");
