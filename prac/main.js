9aa525d010cca9d96aa11fa6719b7a564f39cb45
Onam Kwon <kon6443@gmail.com>
2022-11-16 14:23:15 +0900

    Practiced clean code

--- prac/main.js
+++ prac/main.js
@@ -5,14 +5,14 @@
  * 1. 1부터 10까지 순회하면서 숫자를 출력하세요.
  * 2. 3, 4, 7 에서는 숫자와 함께 "Hello, world!"를 출력하세요.
  * 3. 7에서는 "Hello, world!" 대신 "Bye, world!"가 출력되도록 수정하세요.
- * 4. 4에서는 숫자가 출력되지 않게 수정하세요
+ * 4. 4에서는 숫자가 출력되지 않게 수정하세요.
 */
 // 코드 작성
 
 const caseMap = {
     3: "Hello World, 3!",
-    4: "Hello World!",
-    7: "Bye, World!",
+    4: "",
+    7: "Bye, World!"
 }
 
 function printGreetingMessage(caseNumber) {
